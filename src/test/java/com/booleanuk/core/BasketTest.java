eae659ab3917b06144d72e5f2c7f0a7522a82edd
Dag Andreas <daanfofo@gmail.com>
2024-08-14 11:15:43 +0200

    red testadd

---
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -0,0 +1,24 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BasketTest {
+    public Basket getTestBasket(){
+        return new Basket();
+    }
+
+
+    @Test
+    public void testBasketConstructor(){
+        Basket basket = getTestBasket();
+    }
+
+    @Test
+    void testadd(){
+        Basket basket = getTestBasket();
+        basket.add("Banana", 10);
+        basket.add("Coca Cola", 20);
+        Assertions.assertTrue(basket.items.contains("Banana"));
+    }
+}
